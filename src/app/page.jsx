822fb3d7fd8dccf549b55b6ef2cf9b4721b28b67
mofetaiwo <87393730+mofetaiwo@users.noreply.github.com>
2024-01-27 17:59:07 +0000

    updated home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,8 +12,8 @@ export default function Home() {
             <p className="py-6">
             Automated recurring appointments on the calendar, personalised suggestions for journaling prompts, dinner ideas, and events. Connect with others to plan meet ups, receive reminders, and get calendar suggestions. 
             </p>
-            <Link href="/login" className="btn btn-primary">Log In </Link>
-            <Link href="/signup" className="btn btn-secondary">Sign Up </Link>
+            <Link href="/login" className="btn btn-info btn-primary">Log In </Link>
+            <Link href="/signup" className="btn btn-info btn-secondary">Sign Up </Link>
 
           </div>
         </div>
