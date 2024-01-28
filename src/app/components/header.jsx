a865db02747683e06c8a710e98fbaeb6098ef0a3
alexlampe14 <lampealex888@gmail.com>
2024-01-27 21:05:01 -0500

    fix middleware error

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,6 +1,5 @@
 "use client";
-import styles from "../globals.css";
-
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -28,7 +27,7 @@ export default function Header() {
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a>Home</a>
+              <Link href="/weather">Weather</Link>
             </li>
             <li>
               <a>Help</a>
--- src/app/weather/page.jsx
+++ src/app/weather/page.jsx
@@ -1,61 +1,56 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
+const Weath = dynamic(() => import("../components/weather"), {
 function WeatherPage() {
   const [weather, setWeather] = useState("");
   const [city, setCity] = useState("");
-  // const apiKey = process.env.WEATHER_API_KEY;
-  // const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-  // const apiKey = "0adfa1ef116b49c1b93214506242701";
-  const apiKey = process.env.WEATHER_API_KEY;
-  
+  const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 
   const getWeather = async (e) => {
-    console.log(apiKey);
-    console.log("Processenv" + process.env);
     e.preventDefault();
     const loc = e.target.elements.loc.value;
     const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${loc}`;
-    const req = axios.get(url);
-    const res = await req;
-    setWeather({
-      descp: res.current.condition.text,
-      temp: res.current.temp_f,
-      city: res.location.name,
-      humidity: res.current.humidity,
-      press: res.current.pressure_in,
-    });
-
-    setCity(res.data.name);
+    try {
+      const res = await axios.get(url);
+      setWeather({
+        descp: res.current.condition.text,
+        temp: res.current.temp_f,
+        city: res.location.name,
+        humidity: res.current.humidity,
+        press: res.current.pressure_in,
+      });
+      setCity(res.data.name);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
   };
 
   const Weath = () => {
     return (
       <div>
-        <div className="winfo">
+        <div className="">
           Weather information for {city}
-          <hr></hr>
+          <hr />
         </div>
-        <div className="Weath">
-          <div className="welement">Weather : {weather.descp}</div>
-          <div className="welement">Temperature : {weather.temp}</div>
-          <div className="welement">Humidity :{weather.humidity} %</div>
-          <div className="welement">Pressure : {weather.press} mb</div>
+        <div className="">
+          <div className="">Weather : {weather.descp}</div>
+          <div className="">Temperature : {weather.temp}</div>
+          <div className="">Humidity :{weather.humidity} %</div>
+          <div className="">Pressure : {weather.press} mb</div>
         </div>
       </div>
     );
   };
   return (
     <>
-      <div className="weathhead">Weather Info</div>
-      <div className="mainweather">
-        <div className="weather">
-          <form onSubmit={getWeather} className="form">
+      <div className="">Weather Info</div>
+      <div className="">
+        <div className="">
+          <form onSubmit={getWeather} className="">
             <input type="text" placeholder="city" name="loc" />
-            <button className="bttn">Search</button>
+            <button className="">Search</button>
           </form>
-
           {weather && <Weath />}
         </div>
       </div>
